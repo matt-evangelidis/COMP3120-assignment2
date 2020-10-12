efe94eacaf3ac816861f71f2f9e1128f7c59abce
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-10-12 11:29:17 +1100

    Added template dom router structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import "./styles/base.scss"
+import "./styles/base.scss";
 
-import React from 'react';
+import React from "react";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-        <h1 id="hw">Hello <span className="world">World</span></h1>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Switch>
+					<Route path="/">
+						<h1 id="hw">
+							Hello <span className="world">World</span>
+						</h1>
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
