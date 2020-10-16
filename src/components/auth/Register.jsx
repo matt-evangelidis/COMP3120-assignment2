f6ed641930e155308760eccffc36a16b43e2666d
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-10-16 21:17:10 +1100

    Commented out style code in favour of external stylesheet

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -3,19 +3,17 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
-import { withStyles } from "@material-ui/core/styles";
+// import { withStyles } from "@material-ui/core/styles";
 
-const styles = {
-	textField: {
-		width: "100%",
-		marginBottom: 5,
-	},
-	btnBlock: {
-		textAlign: "center",
-		marginBottom: 10,
-		marginTop: 15,
-	},
-};
+// const styles = {
+// 	textField: {
+// 		width: "100%",
+// 		marginBottom: 5,
+// 	},
+// 	btnBlock: {
+// 		textAlign: "center",
+// 	},
+// };
 
 class Register extends React.Component {
 	constructor(props) {
@@ -51,50 +49,62 @@ class Register extends React.Component {
 		const { classes } = this.props;
 		const { errors } = this.state;
 
+		/* I removed the 'Paper' element because I think stuff like paper or cards should not be part of the actual component
+        instead the something like 'Paper' would be wrapped around this component when it is usedOtherwise if we wanted to use 
+        the register menu in a different place/context it might be awkward */
 		return (
-			<Paper style={{ padding: 15 }}>
-				{/* I think stuff like paper or cards should not be part of the actual component
-                instead the something like 'Paper' would be wrapped around this component when it is used
-                Otherwise if we wanted to use the register menu in a different place/context it might be awkward */}
-				<form onSubmit={this.handleSubmit}>
-					<TextField
-						type="username"
-						label="Username"
-						name="username"
-						className={classes.textField}
-						value={this.state.username}
-						onChange={this.handleChange}
-						helperText={errors.username ? errors.username : ""}
-						error={errors.username ? true : false}
-					/>
-					<TextField
-						type="password"
-						label="Password"
-						name="password"
-						className={classes.textField}
-						value={this.state.password}
-						onChange={this.handleChange}
-						helperText={errors.password ? errors.password : ""}
-						error={errors.password ? true : false}
-					/>
-					<TextField
-						type="password"
-						label="Repeat Password"
-						name="password2"
-						className={classes.textField}
-						value={this.state.password2}
-						onChange={this.handleChange}
-						helperText={errors.password2 ? errors.password2 : ""}
-						error={errors.password2 ? true : false}
-					/>
+			<form onSubmit={this.handleSubmit}>
+				<TextField
+					type="username"
+					label="Username"
+					name="username"
+					// className={classes.textField}
+					value={this.state.username}
+					onChange={this.handleChange}
+					helperText={errors.username ? errors.username : ""}
+					error={errors.username ? true : false}
+					variant="outlined"
+					fullWidth
+				/>
+				<TextField
+					type="password"
+					label="Password"
+					name="password"
+					// className={classes.textField}
+					value={this.state.password}
+					onChange={this.handleChange}
+					helperText={errors.password ? errors.password : ""}
+					error={errors.password ? true : false}
+					variant="outlined"
+					fullWidth
+				/>
+				<TextField
+					type="password"
+					label="Repeat Password"
+					name="password2"
+					// className={classes.textField}
+					fullWidth
+					value={this.state.password2}
+					onChange={this.handleChange}
+					helperText={errors.password2 ? errors.password2 : ""}
+					error={errors.password2 ? true : false}
+					variant="outlined"
+				/>
 
-					<div className={classes.btnBlock}>
-						<Button variant="outlined" type="submit">
-							Register!
-						</Button>
-					</div>
-				</form>
-			</Paper>
+				<div
+				// className={classes.btnBlock}
+				>
+					<Button
+						variant="contained"
+						type="submit"
+						fullWidth
+						color="primary"
+						disableElevation
+					>
+						Register
+					</Button>
+				</div>
+			</form>
 		);
 	}
 }
