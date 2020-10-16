9140c81f29f3cb7109a350c2c0597d53232efe40
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-10-16 21:16:49 +1100

    Swapped primary and secondary colours

--- src/Theme.jsx
+++ src/Theme.jsx
@@ -3,14 +3,14 @@ import { createMuiTheme } from "@material-ui/core/styles";
 const theme = createMuiTheme({
 	palette: {
 		primary: {
-			light: "#ff6e40",
-			main: "#ff3d00",
-			dark: "#bf360c",
-		},
-		secondary: {
 			light: "#86C6FD",
 			main: "#1E96FC",
 			dark: "#0257A1",
+		},
+		secondary: {
+			light: "#ff6e40",
+			main: "#ff3d00",
+			dark: "#bf360c",
 		},
 		text: {
 			primary: "#000000dd",
