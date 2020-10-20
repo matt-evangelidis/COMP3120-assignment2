845a9ebc9d709958b6d8bf30e0bedbfb222f2aa0
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-10-20 15:24:06 +1100

    Fixed syntax error with 'express.json()'

--- server/server.js
+++ server/server.js
@@ -2,22 +2,22 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
 
-//routers and similar can go here
+//# routers and similar can go here
 const usersRouter = require("./routes/users.js");
 
-//app.use statements
+//# app.use statements
+app.use(express.json());
 app.use(cors());
-app.use(express.json);
 app.use("/api/users", usersRouter);
 
-app.get("/*", (req, res) => {
+app.get("/", (req, res) => {
 	res.send("<h1>Hello World, but this time it came from the server</h1>");
 });
 
 const PORT = process.env.BACKEND_PORT || process.env.PORT || 3001;
-console.log(PORT);
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
