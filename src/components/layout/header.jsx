50c74c4cdc07289ea68777157a1cd71fa59485df
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-10-24 12:36:15 +1100

    Fixed typo

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,4 +1,4 @@
-Impimport React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
