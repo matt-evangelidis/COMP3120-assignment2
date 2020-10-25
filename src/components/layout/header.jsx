37ea0e68fb132fccb77062c09bef229e18455165
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-10-25 12:40:11 +1100

    Removed Unused code

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -4,48 +4,25 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
-import Icon from "@material-ui/core/Icon";
 import Typography from "@material-ui/core/Typography";
-import Drawer from "@material-ui/core/Drawer";
 
 function Header(props) {
-	const [drawerStatus, setDrawerStatus] = useState(false);
-
-	const drawerClickOut = (event) => {
-		if (!event.target.classList.contains("drawer-body")) {
-			setDrawerStatus(false);
-		}
-	};
-
 	return (
-		<>
-			<AppBar className="nav-bar" position="static">
-				<Toolbar style={{ paddingLeft: 0 }}>
-					{/* <IconButton onClick={() => setDrawerStatus(!drawerStatus)}>
+		<AppBar className="nav-bar" position="static">
+			<Toolbar style={{ paddingLeft: 0 }}>
+				{/* <IconButton onClick={() => setDrawerStatus(!drawerStatus)}>
 						<Icon>menu</Icon>
 					</IconButton> */}
-					<Typography variant="h6">Flex-Sheet</Typography>
-					<Button component={Link} to="/" style={{ margin: "auto" }}>
-						Characters
-					</Button>
-					<Button component={Link} to="/login" style={{ marginRight: 0 }}>
-						Login
-					</Button>
-				</Toolbar>
-			</AppBar>
-
-			{/* <Drawer
-				open={drawerStatus}
-				PaperProps={{
-					className: "drawer-body",
-				}}
-				onClick={drawerClickOut}
-			>
-				This is the drawer
-			</Drawer> */}
-		</>
+				<Typography variant="h6">Flex-Sheet</Typography>
+				<Button component={Link} to="/" style={{ margin: "auto" }}>
+					Characters
+				</Button>
+				<Button component={Link} to="/login" style={{ marginRight: 0 }}>
+					Login
+				</Button>
+			</Toolbar>
+		</AppBar>
 	);
 }
 
