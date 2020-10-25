ba677b68354a37d168469aa779ea192726da6e5b
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-10-25 12:47:45 +1100

    Removed unused components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import "./styles/base.scss";
 import "./styles/forms.scss";
 
+import React, { useState } from "react";
 
-import React, {useState} from 'react';
 import "material-design-icons/iconfont/material-icons.css";
 
 import "react-grid-layout/css/styles.css";
 import "react-resizable/css/styles.css";
-
-import React, { useState } from "react";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
@@ -17,10 +15,6 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Paper from "@material-ui/core/Paper";
 
 import theme from "./Theme";
-import Login from "./components/LoginForm";
-//import Login from "./components/auth/Login";
-import Register from "./components/auth/Register";
-import Button from './components/Button'
 
 import Header from "./components/layout/header";
 
@@ -30,17 +24,17 @@ import Sheet from "./components/flex-sheet/Sheet";
 
 function App() {
 	const formCardStyle = { width: "700px", margin: "auto" };
-	const [user, setUser] = useState(null) //Keep track of user login status
+	const [user, setUser] = useState(null); //Keep track of user login status
 
 	//handle logout
 	const logoutHandler = () => {
-		setUser(null)
-	  }
-	
+		setUser(null);
+	};
+
 	//handle user login
 	const userLoginHandler = (user) => {
-		setUser(user)
-	}
+		setUser(user);
+	};
 
 	return (
 		<ThemeProvider theme={theme}>
@@ -50,30 +44,6 @@ function App() {
 			{/*This eliminates visual discrepancies caused by differing default browser stylesheets */}
 			<Router>
 				<div className="App">
-				<div>
-					{user !== null
-					? <span>
-						<em> {user.name} logged in </em> 
-						<Button eventHandler={logoutHandler} text="logout"/>
-						</span>
-					: <Link to="/login">Login Page</Link>
-					}
-				</div>
-					<Switch>
-						<Route path="/login">
-              				<Login user={user} userLoginHandler={userLoginHandler}/>
-           				</Route>
-						<Route path="/register">
-							<Paper style={formCardStyle}>
-								<Register />
-							</Paper>
-						</Route>
-						<Route path="/">
-							<Paper style={formCardStyle}>
-								<Login />
-							</Paper>
-						</Route>
-					</Switch>
 					<Header />
 					<div className="content">
 						<Switch>
@@ -84,7 +54,7 @@ function App() {
 							</Route>
 							<Route path="/login">
 								<Paper style={formCardStyle}>
-									<Login />
+									<Login user={user} setUserFn={setUser} />
 								</Paper>
 							</Route>
 							<Route path="/">
