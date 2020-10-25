0c2f427a25aec259c7bf2f0fa1515ef4a336f45c
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-10-25 17:15:55 +1100

    Fixed password comparison error in login route

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,20 +9,21 @@ const validateLoginInput = require("../validation/login");
 
 //# login user if the correct information is used
 router.route("/login").post(async (req, res) => {
-	// const { errors, isValid } = validateLoginInput(req.body);
-
-	// if (!isValid) {
-	// 	return res.status(400).json(errors);
-	// }
+	console.log("Received request to login");
 
 	const body = req.body;
 
 	//# Search for user from database by the username attached in req
 	const user = await User.findOne({ username: body.username });
+	// const passwordCorrect =
+	// 	user === null
+	// 		? false
+	// 		: await bcrypt.compare(body.password, user.passwordHash);
+	//Check the password attached in req
+
 	const passwordCorrect =
-		user === null
-			? false
-			: await bcrypt.compare(body.password, user.passwordHash); //Check the password attached in req
+		user === null ? false : body.password === user.password;
+	//? Temporary tweak to password comparison as currently the database is storing raw passwords and not encrypted passwords
 
 	if (!(user && passwordCorrect)) {
 		return res.status(401).json({ error: "invalid username or password" }); //401 - unauthorized if username or password is incorrect
@@ -43,9 +44,12 @@ router.route("/login").post(async (req, res) => {
 });
 
 router.route("/:username").get((req, res) => {
+	console.log("Received request to fetch user info");
 	const username = req.params.username;
 });
 
-router.route("/").get((req, res) => {});
+router.route("/").get((req, res) => {
+	console.log("Received get request to root users api url");
+});
 //
 module.exports = router;
