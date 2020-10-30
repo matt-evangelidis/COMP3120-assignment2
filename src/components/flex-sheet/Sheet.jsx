06297f1f43216842c78c7958cda8620304bd3eb2
Superdoedoe <39117493+Superdoedoe@users.noreply.github.com>
2020-10-30 19:31:14 +1100

    Add Note Button

--- src/components/flex-sheet/Sheet.jsx
+++ src/components/flex-sheet/Sheet.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import GridLayout, { WidthProvider } from "react-grid-layout";
+import Button from "@material-ui/core/Button";
 
 import Note from "./Note";
 
@@ -15,33 +16,38 @@ function Sheet(props) {
 	//     }
 	// }, []);
 
-	function Test() {
-		return (
-			<div key={4}>
-				<Note></Note>
-			</div>
-		);
+	const [notes, setNotes] = useState([]);
+	const [noteSequencer, setNoteSequencer] = useState(0);
+
+	function addNote() {
+		setNotes([...notes, noteSequencer])
+		setNoteSequencer(noteSequencer + 1)
+	}
+	
+	function handleLayoutChange(layout) {
+		console.log(layout)
 	}
 
 	return (
-		<GridLayout
-			cols={12}
-			rowHeight={30}
-			width={window.screen.width}
-			verticalCompact={false}
-			resizeHandle={<div></div>}
-		>
-			<div key={1}>
-				<Note></Note>
-			</div>
-			<div key={2}>
-				<Note></Note>
-			</div>
-			<div key={3}>
-				<Note></Note>
-			</div>
-			<Test></Test>
-		</GridLayout>
+		<div>
+			<Button onClick={addNote} >Add note</Button>
+			<GridLayout
+				cols={12}
+				rowHeight={30}
+				width={window.screen.width}
+				verticalCompact={false}
+				resizeHandle={<div></div>}
+				onLayoutChange={handleLayoutChange}
+			>
+				{
+					notes.map((note) =>
+						<div key={note}>
+							<Note></Note>
+						</div>
+					)
+				}
+			</GridLayout>
+		</div>
 
 		//? I trued moving the 'div' wrappers into the 'Note' component but that caused the resize handles to not show up for some reason
 	);
