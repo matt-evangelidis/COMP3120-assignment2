343995068a1553d7d0842bbb3c58a19a632b5dd4
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-10-30 20:05:49 +1100

    Removed unused code

--- src/components/flex-sheet/Sheet.jsx
+++ src/components/flex-sheet/Sheet.jsx
@@ -1,36 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-import GridLayout, { WidthProvider } from "react-grid-layout";
+import GridLayout from "react-grid-layout";
 import Button from "@material-ui/core/Button";
 
 import Note from "./Note";
 
-// const GridEl = WidthProvider(GridLayout);
-
 function Sheet(props) {
-	// const [state, setState] = useState('');
-
-	// useEffect(() => {
-	// 	return () => {
-
-	//     }
-	// }, []);
-
 	const [notes, setNotes] = useState([]);
 	const [noteSequencer, setNoteSequencer] = useState(0);
 
 	function addNote() {
-		setNotes([...notes, noteSequencer])
-		setNoteSequencer(noteSequencer + 1)
+		setNotes([...notes, noteSequencer]);
+		setNoteSequencer(noteSequencer + 1);
 	}
-	
+
 	function handleLayoutChange(layout) {
-		console.log(layout)
+		console.log(layout);
 	}
 
 	return (
 		<div>
-			<Button onClick={addNote} >Add note</Button>
+			<Button onClick={addNote}>Add note</Button>
 			<GridLayout
 				cols={12}
 				rowHeight={30}
@@ -39,13 +29,11 @@ function Sheet(props) {
 				resizeHandle={<div></div>}
 				onLayoutChange={handleLayoutChange}
 			>
-				{
-					notes.map((note) =>
-						<div key={note}>
-							<Note></Note>
-						</div>
-					)
-				}
+				{notes.map((note) => (
+					<div key={note}>
+						<Note></Note>
+					</div>
+				))}
 			</GridLayout>
 		</div>
 
