576507f5e60ff5499696b52eb019ee1f6a467131
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-11-02 18:21:33 +1100

    Fixed issue with header button spacing

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -8,14 +8,24 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 function Header(props) {
+	const sheetsLinkStyle = {
+		margin: "auto",
+	};
+
+	const userButtonStyle = {
+		marginRight: 0,
+		marginLeft: props.user ? "unset" : "auto",
+	};
 	return (
 		<AppBar className="nav-bar" position="static">
 			<Toolbar>
 				<Typography variant="h6">Flex-Sheet</Typography>
-				<Button component={Link} to="/sheets" style={{ margin: "auto" }}>
-					Sheets
-				</Button>
-				<Button component={Link} to="/login" style={{ marginRight: 0 }}>
+				{props.user && (
+					<Button component={Link} to="/sheets" style={sheetsLinkStyle}>
+						Sheets
+					</Button>
+				)}
+				<Button component={Link} to="/login" style={userButtonStyle}>
 					{props.user ? props.user.username : "Login"}
 				</Button>
 			</Toolbar>
